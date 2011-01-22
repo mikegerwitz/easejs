dea6972416776e79fc1e953e90e5ced3b1f012aa
Mike Gerwitz <mike@mikegerwitz.com>
2011-01-21 20:54:55 -0500

    Began adding basic method builder (currently operates the same as property builder) - They are essentially the same, but the method builder will have additional logic (e.g. method overriding) and support different keywords

---
+++ test/test-member_builder-method.js
@@ -0,0 +1,34 @@
+/**
+ * Tests method builder
+ *
+ *  Copyright (C) 2010 Mike Gerwitz
+ *
+ *  This file is part of ease.js.
+ *
+ *  ease.js is free software: you can redistribute it and/or modify it under the
+ *  terms of the GNU Lesser General Public License as published by the Free
+ *  Software Foundation, either version 3 of the License, or (at your option)
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT
+ *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ *  for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author  Mike Gerwitz
+ * @package test
+ */
+
+var common    = require( './common' ),
+    mb_common = require( './inc-member_builder-common' )
+;
+
+mb_common.value       = function() {};
+mb_common.buildMember = common.require( 'member_builder' ).buildMethod;
+
+// do assertions common to all member builders
+mb_common.assertCommon();
+
