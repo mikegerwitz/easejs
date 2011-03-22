97036e4c8844801c728b46e891fd747fc59044ef
Mike Gerwitz <mike@mikegerwitz.com>
2011-03-21 22:01:15 -0400

    Added jQuery to page using interactive.js; does not yet do anything with page

--- doc/interactive.js
+++ doc/interactive.js
@@ -1,2 +1,25 @@
+/**
+ * Makes HTML documentation interactive
+ */
 
-// placeholder
+var head       = document.getElementsByTagName( 'head' )[ 0 ],
+    sjquery    = document.createElement( 'script' ),
+    sjquery_ui = document.createElement( 'script' ),
+    css        = document.createElement( 'link' );
+
+sjquery.type = sjquery_ui.type = 'text/javascript';
+
+css.type = 'text/css';
+css.rel  = 'stylesheet';
+
+sjquery.src  =
+    'https://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js';
+sjquery_ui.src =
+    'https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.11/jquery-ui.min.js';
+css.href =
+    'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.10/themes/base/jquery-ui.css';
+
+head.appendChild( sjquery );
+head.appendChild( sjquery_ui );
+head.appendChild( css );
+
