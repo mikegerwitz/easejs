f2b7ecb4a648463c1ae13e584a2c962f287d2701
Mike Gerwitz <mike@mikegerwitz.com>
2011-11-05 13:04:55 -0400

    Added to class example

--- scripts/ex/class.js
+++ scripts/ex/class.js
@@ -2,13 +2,28 @@
  * Basic class declaration example
  */
 
+// Define class Dog
 var Dog = Class( 'Dog',
 {
+    'private _name': '',
+
+
+    __construct: function( name )
+    {
+        this._name = name;
+    },
+
+
     'public bark': function()
     {
-        console.log( 'Woof!' );
+        console.log( this._name + ' says: Woof!' );
     }
 } );
 
-Dog().bark();
+// invoke method 'bark' on a new instance of 'Dog'
+Dog( 'Fluffy' ).bark();
+
+// alternatively, we can use the 'new' keyword
+var inst = new Dog( 'Rompie' );
+inst.bark();
 
