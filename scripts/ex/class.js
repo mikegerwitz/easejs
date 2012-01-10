45cd68908b3eb242605dfee9fecbdcab2feb93ef
Mike Gerwitz <mike@mikegerwitz.com>
2012-01-09 22:41:57 -0500

    Removed header from class declaration example

--- scripts/ex/class.js
+++ scripts/ex/class.js
@@ -1,18 +1,12 @@
-/**
- * Basic class declaration example
- */
-
-// Define class Dog
+// define class Dog
 var Dog = Class( 'Dog',
 {
     'private _name': '',
-
 
     __construct: function( name )
     {
         this._name = name;
     },
-
 
     'public bark': function()
     {
