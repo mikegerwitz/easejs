97fbbd5bb927b204ed41b9c0417ae2e1c0dfb9a3
Mike Gerwitz <mikegerwitz@gnu.org>
2014-01-15 23:56:00 -0500

    [no-copyright] Modified headers to reduce GPL license notice width

--- tools/combine-order.js
+++ tools/combine-order.js
@@ -1,8 +1,8 @@
 /**
  * Determines dependency order
  *
- * This script will determine the order in which files must be concatenated in
- * order to run properly. Dependencies must be added before the file that
+ * This script will determine the order in which files must be concatenated
+ * in order to run properly. Dependencies must be added before the file that
  * depends on them.
  *
  * Circular dependencies are not supported, nor should they be.
@@ -11,18 +11,18 @@
  *
  *  This file is part of GNU ease.js.
  *
- *  ease.js is free software: you can redistribute it and/or modify it under the
- *  terms of the GNU General Public License as published by the Free Software
- *  Foundation, either version 3 of the License, or (at your option) any later
- *  version.
+ *  ease.js is free software: you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free
+ *  Software Foundation, either version 3 of the License, or (at your
+ *  option) any later version.
  *
- *  This program is distributed in the hope that it will be useful, but WITHOUT
- *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- *  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
- *  more details.
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License along with
- *  this program.  If not, see <http://www.gnu.org/licenses/>.
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * @author  Mike Gerwitz
  */
