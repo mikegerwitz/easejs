017b85d75bd5b3afb22672f111f270921931dc36
Mike Gerwitz <mtg@gnu.org>
2016-12-29 03:11:49 -0500

    Copyright year updates for recent trait changes
    * lib/Trait.js: Add Copyright year 2016.
    * test/Trait/ContextTest.js: Add Copyright year 2016.

--- test/Trait/ContextTest.js
+++ test/Trait/ContextTest.js
@@ -1,7 +1,7 @@
 /**
  * Tests trait scoping
  *
- *  Copyright (C) 2014 Free Software Foundation, Inc.
+ *  Copyright (C) 2014, 2016 Free Software Foundation, Inc.
  *
  *  This file is part of GNU ease.js.
  *
